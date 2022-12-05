2193a8f42c2e1af433528fb1b1f58dd6466ab2d5
hengage <tunecarterkillz@gmail.com>
2022-12-05 17:46:42 +0100

    Feature: transaction invoice schema function

--- Controller/invoice/invoiceSchema.js
+++ Controller/invoice/invoiceSchema.js
@@ -1,38 +1,25 @@
 const Transaction = require('../../models/transaction')
 
-// const findTransanction = (req, res) => {
-// 	const transactionId = Transaction.findOne({transactId: req.body.transactId})
-// 	console.log(transactionId)
-// 	return transactionId
-// }
-// findTransanction()
-
-const invoiceSchema = {
-	senderDetails: {
-		name: 'John Doe',
-		address: '1234 Main Street',
-		city: 'San Francisco',
-		state: 'CA',
-		country: 'US',
-		postal_code: 94111,
-	},
-	items: [
-		{
-			item: 'TC 100',
-			description: 'Toner Cartridge',
-			quantity: 2,
-			amount: 6000,
+const transactionInvoiceSchema = () => {
+	const transaction = Transaction.findOne({transactId: req.body.transactId})
+	
+	const invoiceSchema = {
+		senderDetails: {
+			senderEmail: transaction.transactionUserEmail
 		},
-		{
-			item: 'USB_EXT',
-			description: 'USB Cable Extender',
-			quantity: 1,
-			amount: 2000,
+		receiverDetails: {
+			receiverEmail: transaction.recieverUserEmail
 		},
-	],
-	subtotal: 8000,
-	paid: 0,
-	invoice_nr: 1234,
-};
+		transactionDetails: {
+			amountSent: transaction.Sent,
+			accountCharge: transaction.acccountCharge,
+			transactId: transaction.transactId,
+		}
+	}
 
-module.exports = invoiceSchema;
+	return invoiceSchema
+}
+
+module.exports = transactionInvoiceSchema;
+
+
