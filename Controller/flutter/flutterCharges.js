5e79a884479a6e1a69520ae2d5f5a74dea12180d
unknown <hardeyshinnah.mo@gmail.com>
2022-12-15 00:38:54 -0800

    15th commit flutter

--- Controller/flutter/flutterCharges.js
+++ Controller/flutter/flutterCharges.js
@@ -30,88 +30,51 @@ const flutterCharges = async (payload) =>{
             cardtype=response.data.card.type
             
 
-            {
-                "event": "charge.completed",
-                "data": {
-                  "id": 285959875,
-                  "tx_ref": "Links-616626414629",
-                  "flw_ref": "PeterEkene/FLW270177170",
-                  "device_fingerprint": "a42937f4a73ce8bb8b8df14e63a2df31",
-                  "amount": 100,
-                  "currency": "NGN",
-                  "charged_amount": 100,
-                  "app_fee": 1.4,
-                  "merchant_fee": 0,
-                  "processor_response": "Approved by Financial Institution",
-                  "auth_model": "PIN",
-                  "ip": "197.210.64.96",
-                  "narration": "CARD Transaction ",
-                  "status": "successful",
-                  "payment_type": "card",
-                  "created_at": "2020-07-06T19:17:04.000Z",
-                  "account_id": 17321,
-                  "customer": {
-                    "id": 215604089,
-                    "name": "Yemi Desola",
-                    "phone_number": null,
-                    "email": "user@gmail.com",
-                    "created_at": "2020-07-06T19:17:04.000Z"
-                  },
-                  "card": {
-                    "first_6digits": "123456",
-                    "last_4digits": "7889",
-                    "issuer": "VERVE FIRST CITY MONUMENT BANK PLC",
-                    "country": "NG",
-                    "type": "VERVE",
-                    "expiry": "02/23"
-                  }
-                }
-              }
+            
 
-
-            }
+         
             const newFlutterWebHook =  new flutterWebHook(
                 {
-                    event:response.event              
-                    id: response.data.id
-                    tx_ref: { type: String, required: true, unique: true},
-                    flw_ref: { type: String, required: true, unique: true},
-                    device_fingerprint: { type: String, required: true},
-                    amount: { type: Number, required: true},
-                    currency: { type: String, required: true},
-                    charged_amount: { type: String, required: true},
-                    app_fee: { type: String, required: true},
-                    merchant_fee: { type: String, required: true},
-                    processor_response: { type: String, required: true},
-                    auth_model: { type: Number, required: true},
-                    ip: { type: String, required: true},
-                    narration: { type: String, required: true},
-                    status: { type: String, required: true},
-                    payment_type: { type: String, required: true},
-                    created_at: { type: String, required: true},
-                    account_id: { type: Number, required: true},
-                    id: { type: String, required: true},
-                    name: { type: String, required: true},
-                    phone_number: { type: Number, required: true},
-                    email: { type: String, required: true},
-                    created_at: { type: String, required: true},
-                    first_6digits: { type: Number, required: true},
-                    last_4digits: { type: String, required: true},
-                    issuer: { type: String, required: true},
-                    country: { type: String, required: true},
-                    type: { type: String, required: true},
-                    expiry: { type: String, required: true}
+                    event:response.event,              
+                    id: response.data.id,
+                    tx_ref: response.data.tx_ref,
+                    flw_ref: response.data.flw_ref,
+                    device_fingerprint: response.device_fingerprint,
+                    amount: response.data.amount,
+                    currency: response.data.currency,
+                    charged_amount: response.data.charged_amount,
+                    app_fee: response.data.app_fee,
+                    merchant_fee: response.data.merchant_fee,
+                    processor_response: response.data.processor_response,
+                    auth_model: response.data.auth_model,
+                    ip: response.data.ip,
+                    narration: response.data.narration,
+                    status: response.data.status,
+                    payment_type: response.data.payment_type,
+                    created_at: response.data.created_at,
+                    account_id: response.data.account_id,
+                    id: response.data.id,
+                    name: response.data.name,
+                    phone_number: response.data.phone_number,
+                    email: response.data.email,
+                    created_at: response.data.created_at,
+                    first_6digits: response.data.first_6digits,
+                    last_4digits: response.data.last_4digits,
+                    issuer: response.data.issuer,
+                    country: response.data.country,
+                    type: response.data.type,
+                    expiry: response.data.expiry
             
                 }
     
             )
 
             const savetransaction = await newFlutterWebHook.save();
-          
+                res.status(200).json(savetransaction);
                
              
                 } else {
-                    // Inform the customer their payment was unsuccessful
+                    return res.status(400).json('Payment unsuccessful')
                 }
 
            
